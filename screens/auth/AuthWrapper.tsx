41b9a1ce796a60a801faf7d28893c7b9e53800c0
Abhijeet Jagtap <abhijeet@rulezero.com>
2023-02-28 23:47:11 +0530

    Added Page Wrappers

---
+++ screens/auth/AuthWrapper.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import LoginScreen from './components/LoginScreen'
+import SignUpScreen from './components/SignUpScreen'
+
+const AuthWrapper = () => {
+  return <SignUpScreen />
+}
+
+export default AuthWrapper
---
+++ screens/home/HomeWrapper.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HomeWrapper = () => {
+  return (
+    <View>
+      <Text>HomeWrapper</Text>
+    </View>
+  )
+}
+
+export default HomeWrapper
